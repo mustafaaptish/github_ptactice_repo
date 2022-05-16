649b8841cd06de88eb890ed8a10bdbcf694602af
Mustafa Aptish <mustafaaptish@gmail.com>
2022-05-16 21:16:59 +0100

    updated

--- src/practice/ThirdJavaClass.java
+++ src/practice/ThirdJavaClass.java
@@ -1,9 +1,17 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThirdJavaClass {
     public static void main(String[] args) {
 
         System.out.println("Github is hard");
+
+
+        List<String> myList = new ArrayList<>();
+        myList.add("Hello");
+
 
     }
 
